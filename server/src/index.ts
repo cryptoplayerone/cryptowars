9a6605c53e87a221cfea5858d7e51baf3b2d25ce
Loredana <loredana.cirstea@gmail.com>
2018-12-07 18:27:06 +0800

    Add temporary lax cors options

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,16 @@ export {CryptowarsApplication};
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new CryptowarsApplication(options);
+  options.rest = {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      maxAge: 86400,
+      credentials: true,
+    },
+  };
   await app.boot();
   await app.start();
 
